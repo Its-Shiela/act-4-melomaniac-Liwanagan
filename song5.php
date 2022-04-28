6e00b474adcb7ffbd89012e8cd3e57234ade9408
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-04-28 14:49:16 -0900

    song 5 done

--- song5.php
+++ song5.php
@@ -18,11 +18,11 @@
                 <button method="GET" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Paper Rings</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Spring Day</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Asan Ka Na Ba?</a></li>
+                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Sincerity is Scary</a></li>
+                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Right Here</a></li>
                     </ul>
                 </div>
             </div>
@@ -33,59 +33,63 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-light">RIGHT HERE</h2>
-                        <p style="color:white;" class="lead">Keshi</p>
-                        <p style="color:white;">I think some words are overdue</p>
-                        <p style="color:white;">Could we just do it over?</p>
-                        <p style="color:white;">Can we just talk it out like friends,</p>
-                        <p style="color:white;">Because I need your shoulder?</p>
-                        <p style="color:white;">I know we ended on the wrong terms</p>
-                        <p style="color:white;">But I said we're past it</p>
-                        <p style="color:white;">So why you texting me with questions</p>
-                        <p style="color:white;">You don't gotta ask me, like:</p>
-                        <p style="color:white;">"I know it's random, how you been?</p>
-                        <p style="color:white;">Do you remember 'bout this band you said you listened to?</p>
-                        <p style="color:white;">When we were younger</p>
-                        <p style="color:white;">When we were softer</p>
-                        <p style="color:white;">When we were all about each other</p>
-                        <p style="color:white;">Hope life is treating you better</p>
-                        <p style="color:white;">Better, better"</p>
-                        <p style="color:white;">Girl, what's with that?</p>
-                        <p style="color:white;">Yo, cut the act</p>
-                        <p style="color:white;">'Cause I don't got time for laughs</p>
-                        <p style="color:white;">No, I do not want the past</p>
-                        <p style="color:white;">But if you are ever in need</p>
-                        <p style="color:white;">And God has you down on your knees</p>
-                        <p style="color:white;">And you do not know who to be</p>
-                        <p style="color:white;">Then go on and come home to me</p>
-                        <p style="color:white;">Because I'm here</p>
-                        <p style="color:white;">If you need me I'll be here</p>
-                        <p style="color:white;">Right here</p>
-                        <p style="color:white;">Said I'm here</p>
-                        <p style="color:white;">If you need me I'll be here</p>
-                        <p style="color:white;">Right here, oh</p>
-                        <p style="color:white;">And how you liking this new guy?</p>
-                        <p style="color:white;">I know you always want the new life</p>
-                        <p style="color:white;">I hope you taking care of you, like</p>
-                        <p style="color:white;">The way you cared for me in my time</p>
-                        <p style="color:white;">See you got yourself some new friends</p>
-                        <p style="color:white;">No, you don't need to go to Houston</p>
-                        <p style="color:white;">You got me thinking back about then</p>
-                        <p style="color:white;">Girl, I wonder why you texting</p>
-                        <p style="color:white;">Girl, what's with that?</p>
-                        <p style="color:white;">Yo, cut the act</p>
-                        <p style="color:white;">'Cause I don't got time for laughs</p>
-                        <p style="color:white;">No, I do not want the past</p>
-                        <p style="color:white;">But if you are ever in need</p>
-                        <p style="color:white;">And God has you down on your knees</p>
-                        <p style="color:white;">And you do not know who to be</p>
-                        <p style="color:white;">Then go on and come home to me</p>
-                        <p style="color:white;">Because I'm here</p>
-                        <p style="color:white;">If you need me I'll be here</p>
-                        <p style="color:white;">Right here</p>
-                        <p style="color:white;">Said I'm here</p>
-                        <p style="color:white;">If you need me I'll be here</p>
-                        <p style="color:white;">Right here, oh</p>
+
+                        <?php
+                        echo "<h2 class='text-light'>RIGHT HERE</h2>";
+                        echo "<p style='color:white;' class='lead'>Keshi</p>";
+                        echo "<p style='color:white;'>I think some words are overdue</p>";
+                        echo "<p style='color:white;'>Could we just do it over?</p>";
+                        echo "<p style='color:white;'>Can we just talk it out like friends,</p>";
+                        echo "<p style='color:white;'>Because I need your shoulder?</p>";
+                        echo "<p style='color:white;'>I know we ended on the wrong terms</p>";
+                        echo "<p style='color:white;'>But I said we're past it</p>";
+                        echo "<p style='color:white;'>So why you texting me with questions</p>";
+                        echo "<p style='color:white;'>You don't gotta ask me, like:</p>";
+                        echo "<p style='color:white;'>'I know it's random, how you been?'</p>";
+                        echo "<p style='color:white;'>'Do you remember 'bout this band you said you listened to?'</p>";
+                        echo "<p style='color:white;'>'When we were younger'</p>";
+                        echo "<p style='color:white;'>'When we were softer'</p>";
+                        echo "<p style='color:white;'>'When we were all about each other'</p>";
+                        echo "<p style='color:white;'>'Hope life is treating you better'</p>";
+                        echo "<p style='color:white;'>'Better, better'</p>";
+                        echo "<p style='color:white;'>Girl, what's with that?</p>";
+                        echo "<p style='color:white;'>Yo, cut the act</p>";
+                        echo "<p style='color:white;'>'Cause I don't got time for laughs</p>";
+                        echo "<p style='color:white;'>No, I do not want the past</p>";
+                        echo "<p style='color:white;'>But if you are ever in need</p>";
+                        echo "<p style='color:white;'>And God has you down on your knees</p>";
+                        echo "<p style='color:white;'>And you do not know who to be</p>";
+                        echo "<p style='color:white;'>Then go on and come home to me</p>";
+                        echo "<p style='color:white;'>Because I'm here</p>";
+                        echo "<p style='color:white;'>If you need me I'll be here</p>";
+                        echo "<p style='color:white;'>Right here</p>";
+                        echo "<p style='color:white;'>Said I'm here</p>";
+                        echo "<p style='color:white;'>If you need me I'll be here</p>";
+                        echo "<p style='color:white;'>Right here, oh</p>";
+                        echo "<p style='color:white;'>And how you liking this new guy?</p>";
+                        echo "<p style='color:white;'>I know you always want the new life</p>";
+                        echo "<p style='color:white;'>I hope you taking care of you, like</p>";
+                        echo "<p style='color:white;'>The way you cared for me in my time</p>";
+                        echo "<p style='color:white;'>See you got yourself some new friends</p>";
+                        echo "<p style='color:white;'>No, you don't need to go to Houston</p>";
+                        echo "<p style='color:white;'>You got me thinking back about then</p>";
+                        echo "<p style='color:white;'>Girl, I wonder why you texting</p>";
+                        echo "<p style='color:white;'>Girl, what's with that?</p>";
+                        echo "<p style='color:white;'>Yo, cut the act</p>";
+                        echo "<p style='color:white;'>'Cause I don't got time for laughs</p>";
+                        echo "<p style='color:white;'>No, I do not want the past</p>";
+                        echo "<p style='color:white;'>But if you are ever in need</p>";
+                        echo "<p style='color:white;'>And God has you down on your knees</p>";
+                        echo "<p style='color:white;'>And you do not know who to be</p>";
+                        echo "<p style='color:white;'>Then go on and come home to me</p>";
+                        echo "<p style='color:white;'>Because I'm here</p>";
+                        echo "<p style='color:white;'>If you need me I'll be here</p>";
+                        echo "<p style='color:white;'>Right here</p>";
+                        echo "<p style='color:white;'>Said I'm here</p>";
+                        echo "<p style='color:white;'>If you need me I'll be here</p>";
+                        echo "<p style='color:white;'>Right here, oh</p>";
+                        ?>
+                        
                         
 
                         
