2d6a2a701cd13ca252fcc722f11227ec151e175a
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-04-28 12:53:04 -0900

    modifying

---
+++ index.php
@@ -0,0 +1,86 @@
+<?php
+session_start();
+if (isset($_REQUEST["song1.php"])=== true){
+
+}
+if (isset($_REQUEST["song2.php"])=== true){
+
+}
+if (isset($_REQUEST["song3.php"])=== true){
+
+}
+if (isset($_REQUEST["song4.php"])=== true){
+
+}
+if (isset($_REQUEST["song5.php"])=== true){
+
+}
+
+
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-transparent fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" style="color: #000" href="#page-top"> Don Quixote </a>
+                <button method="GET" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-image"
+                style = "background-image: url(images/photo_3.jpg);height: 100vh">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Don Quixote</h1>
+                <p class="lead">Music diverge in genre, in tone or in language,</p>
+                <p class="lead">Yet all are euphonious, that makes us dreamers.</p>
+            </div>
+        </header>
+        
+
+                <style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: silver;
+   color: white;
+   text-align: center;
+}
+</style>
+
+        <footer 
+            <div class = "footer"class="container px-4"><p>Copyright &copy; 2022. Shiela Mae Liwanagan</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
