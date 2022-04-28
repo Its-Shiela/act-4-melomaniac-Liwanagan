7bf5e638b672f3bd84077737f2a9df934952de30
LiwanaganShielaMae <gaddimae5@gmail.com>
2022-04-28 13:48:50 -0900

    song 2 modified

--- song2.php
+++ song2.php
@@ -20,11 +20,11 @@
                 <button method="GET" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song1.php">Paper Rings</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song2.php">Spring Day</a></li>
+                        <li class="nav-item"><a class="nav-link text-dark" href="song3.php">Asan Ka Na Ba?</a></li>
+                         <li class="nav-item"><a class="nav-link text-dark" href="song4.php">Sincerity is Scary</a></li>
+                          <li class="nav-item"><a class="nav-link text-dark" href="song5.php">Right Here</a></li>
                     </ul>
                 </div>
             </div>
@@ -35,92 +35,96 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-dark">SPRING DAY (봄날)</h2>
-                        <p class="lead">BTS</p>
-                        <p>Bogo shipda</p>
-                        <p>Ireoke malhanikka deo</p>
-                        <p>Bogo shipda</p>
-                        <p>Neohi sajineul bogo isseodo</p>
-                        <p>Bogo shipda</p>
-                        <p>Neomu yasokhan shigan</p>
-                        <p>Naneun uriga mibda</p>
-                        <p>Ijen eolgul han beon boneun geotto</p>
-                        <p>Himdeureojin uriga</p>
-                        <p>Yeogin ontong gyeoul ppuniya</p>
-                        <p>Palworedo gyeouri wa</p>
-                        <p>Maeumeun shiganeul dallyeogane</p>
-                        <p>Hollo nameun seolgukyeolcha</p>
-                        <p>Ni son jabgo</p>
-                        <p>Jigu bandaepyeonkkaji ga</p>
-                        <p>Gyeoureul kkeutnaegopa</p>
-                        <p>Geuriumdeuri</p>
-                        <p>Eolmana nuncheoreom naeryeoya</p>
-                        <p>Geu bomnari olkka</p>
-                        <p>Friend</p>
-                        <p>Heogongeul tteodoneun</p>
-                        <p style="color:white;">Jageun meonjicheoreom</p>
-                        <p style="color:white;">Jageun meonjicheoreom</p>
-                        <p style="color:white;">Nallineun nuni naramyeon</p>
-                        <p style="color:white;">Jogeum deo ppalli nege</p>
-                        <p>Daheul su isseul tende</p>
-                        <p>Nunkkochi tteoreojyeoyo</p>
-                        <p>Tto jogeumsshik meoreojyeoyo</p>
-                        <p>Bogo shipda (bogo shipda)</p>
-                        <p>Bogo shipda (bogo shipda)</p>
-                        <p>Eolmana gidaryeoya</p>
-                        <p>Tto myeot bameul deo saewoya</p>
-                        <p>Neol boge doelkka (neol boge doelkka)</p>
-                        <p>Mannage doelkka (mannage doelkka)</p>
-                        <p>Chuun gyeoul kkeuteul jina</p>
-                        <p>Dashi bomnari ol ttaekkaji</p>
-                        <p>Kkot piul ttaekkaji</p>
-                        <p>Geugose jom deo meomulleojwo</p>
-                        <p>Meomulleojwo</p>
-                        <p>Niga byeonhan geonji</p>
-                        <p>(Niga byeonhan geonji)</p>
-                        <p>Animyeon naega byeonhan geonji</p>
-                        <p>(Naega byeonhan geonji)</p>
-                        <p>I sungan heureuneun shiganjocha miwo</p>
-                        <p>Uriga byeonhan geoji mwo</p>
-                        <p>Moduga geureon geoji mwo</p>
-                        <p>Geurae mibda niga</p>
-                        <p>Neon tteonatjiman</p>
-                        <p>Dan harudo neoreul</p>
-                        <p>Ijeun jeogi eopseotji nan</p>
-                        <p>Soljikhi bogo shipeunde</p>
-                        <p>Iman neoreul jiulge</p>
-                        <p>Geuge neol wonmanghagibodan</p>
-                        <p style="color:white;">Deol apeunikka</p>
-                        <p style="color:white;">Shirin neol bureonae bonda</p>
-                        <p style="color:white;">Yeongicheoreom hayan yeongicheoreom</p>
-                        <p style="color:white;">Malloneun jiunda haedo</p>
-                        <p>Sashil nan ajik neol bonaeji mothaneunde</p>
-                        <p>Nunkkochi tteoreojyeoyo</p>
-                        <p>Tto jogeumsshik meoreojyeoyo</p>
-                        <p>Bogo shipda (bogo shipda)</p>
-                        <p>Bogo shipda (bogo shipda)</p>
-                        <p>Eolmana gidaryeoya</p>
-                        <p>Tto myeot bameul deo saewoya</p>
-                        <p>Neol boge doelkka (neol boge doelkka)</p>
-                        <p>Mannage doelkka (mannage doelkka)</p>
-                        <p>You know it all</p>
-                        <p>You're my best friend</p>
-                        <p>Achimeun dashi ol geoya</p>
-                        <p>Eotteon eodumdo eotteon gyejeoldo</p>
-                        <p>Yeongweonhal sun eopseunikka</p>
-                        <p>Beotkkochi pinabwayo</p>
-                        <p>I gyeouldo kkeuchi nayo</p>
-                        <p>Bogo shipda (bogo shipda)</p>
-                        <p>Bogo shipda (bogo shipda)</p>
-                        <p>Jogeumman gidarimyeon</p>
-                        <p>Myeochil bamman deo saeumyeon</p>
-                        <p>Mannareo galge (mannareo galge)</p>
-                        <p>Derireo galge (derireo galge)</p>
-                        <p>Chuun gyeoul kkeuteul jina</p>
-                        <p>Dashi bomnari ol ttaekkaji</p>
-                        <p>Kkot piul ttaekkaji</p>
-                        <p>Geugose jom deo meomulleojwo</p>
-                        <p>Meomulleojwo</p>
+
+                        <?php
+                        echo "<h2 class='text-dark'>SPRING DAY (봄날)</h2>";
+                        echo "<p class='lead'>BTS</p>";
+                        echo "<p>Bogo shipda</p>";
+                        echo "<p>Ireoke malhanikka deo</p>";
+                        echo "<p>Bogo shipda</p>";
+                        echo "<p>Neohi sajineul bogo isseodo</p>";
+                        echo "<p>Bogo shipda</p>";
+                        echo "<p>Neomu yasokhan shigan</p>";
+                        echo "<p>Naneun uriga mibda</p>";
+                        echo "<p>Ijen eolgul han beon boneun geotto</p>";
+                        echo "<p>Himdeureojin uriga</p>";
+                        echo "<p>Yeogin ontong gyeoul ppuniya</p>";
+                        echo "<p>Palworedo gyeouri wa</p>";
+                        echo "<p>Maeumeun shiganeul dallyeogane</p>";
+                        echo "<p>Hollo nameun seolgukyeolcha</p>";
+                        echo "<p>Ni son jabgo</p>";
+                        echo "<p>Jigu bandaepyeonkkaji ga</p>";
+                        echo "<p>Gyeoureul kkeutnaegopa</p>";
+                        echo "<p>Geuriumdeuri</p>";
+                        echo "<p>Eolmana nuncheoreom naeryeoya</p>";
+                        echo "<p>Geu bomnari olkka</p>";
+                        echo "<p>Friend</p>";
+                        echo "<p>Heogongeul tteodoneun</p>";
+                        echo "<p style='color:white;'>Jageun meonjicheoreom</p>";
+                        echo "<p style='color:white;'>Jageun meonjicheoreom</p>";
+                        echo "<p style='color:white;'>Nallineun nuni naramyeon</p>";
+                        echo "<p style='color:white;'>Jogeum deo ppalli nege</p>";
+                        echo "<p>Daheul su isseul tende</p>";
+                        echo "<p>Nunkkochi tteoreojyeoyo</p>";
+                        echo "<p>Tto jogeumsshik meoreojyeoyo</p>";
+                        echo "<p>Bogo shipda (bogo shipda)</p>";
+                        echo "<p>Bogo shipda (bogo shipda)</p>";
+                        echo "<p>Eolmana gidaryeoya</p>";
+                        echo "<p>Tto myeot bameul deo saewoya</p>";
+                        echo "<p>Neol boge doelkka (neol boge doelkka)</p>";
+                        echo "<p>Mannage doelkka (mannage doelkka)</p>";
+                        echo "<p>Chuun gyeoul kkeuteul jina</p>";
+                        echo "<p>Dashi bomnari ol ttaekkaji</p>";
+                        echo "<p>Kkot piul ttaekkaji</p>";
+                        echo "<p>Geugose jom deo meomulleojwo</p>";
+                        echo "<p>Meomulleojwo</p>";
+                        echo "<p>Niga byeonhan geonji</p>";
+                        echo "<p>(Niga byeonhan geonji)</p>";
+                        echo "<p>Animyeon naega byeonhan geonji</p>";
+                        echo "<p>(Naega byeonhan geonji)</p>";
+                        echo "<p>I sungan heureuneun shiganjocha miwo</p>";
+                        echo "<p>Uriga byeonhan geoji mwo</p>";
+                        echo "<p>Moduga geureon geoji mwo</p>";
+                        echo "<p>Geurae mibda niga</p>";
+                        echo "<p>Neon tteonatjiman</p>";
+                        echo "<p>Dan harudo neoreul</p>";
+                        echo "<p>Ijeun jeogi eopseotji nan</p>";
+                        echo "<p>Soljikhi bogo shipeunde</p>";
+                        echo "<p>Iman neoreul jiulge</p>";
+                        echo "<p>Geuge neol wonmanghagibodan</p>";
+                        echo "<p style='color:white;'>Deol apeunikka</p>";
+                        echo "<p style='color:white;'>Shirin neol bureonae bonda</p>";
+                        echo "<p style='color:white;'>Yeongicheoreom hayan yeongicheoreom</p>";
+                        echo "<p style='color:white;'>Malloneun jiunda haedo</p>";
+                        echo "<p>Sashil nan ajik neol bonaeji mothaneunde</p>";
+                        echo "<p>Nunkkochi tteoreojyeoyo</p>";
+                        echo "<p>Tto jogeumsshik meoreojyeoyo</p>";
+                        echo "<p>Bogo shipda (bogo shipda)</p>";
+                        echo "<p>Bogo shipda (bogo shipda)</p>";
+                        echo "<p>Eolmana gidaryeoya</p>";
+                        echo "<p>Tto myeot bameul deo saewoya</p>";
+                        echo "<p>Neol boge doelkka (neol boge doelkka)</p>";
+                        echo "<p>Mannage doelkka (mannage doelkka)</p>";
+                        echo "<p>You know it all</p>";
+                        echo "<p>You're my best friend</p>";
+                        echo "<p>Achimeun dashi ol geoya</p>";
+                        echo "<p>Eotteon eodumdo eotteon gyejeoldo</p>";
+                        echo "<p>Yeongweonhal sun eopseunikka</p>";
+                        echo "<p>Beotkkochi pinabwayo</p>";
+                        echo "<p>I gyeouldo kkeuchi nayo</p>";
+                        echo "<p>Bogo shipda (bogo shipda)</p>";
+                        echo "<p>Bogo shipda (bogo shipda)</p>";
+                        echo "<p>Jogeumman gidarimyeon</p>";
+                        echo "<p>Myeochil bamman deo saeumyeon</p>";
+                        echo "<p>Mannareo galge (mannareo galge)</p>";
+                        echo "<p>Derireo galge (derireo galge)</p>";
+                        echo "<p>Chuun gyeoul kkeuteul jina</p>";
+                        echo "<p>Dashi bomnari ol ttaekkaji</p>";
+                        echo "<p>Kkot piul ttaekkaji</p>";
+                        echo "<p>Geugose jom deo meomulleojwo</p>";
+                        echo "<p>Meomulleojwo</p>";
+                        ?>
+                        
 
                     </div>
                 </div>
